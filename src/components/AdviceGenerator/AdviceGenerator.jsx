90966e861b0a45badb0e65dd815831cc171daea1
Raze91 <hattab_sami@yahoo.fr>
2022-11-28 15:21:24 +0100

    first version css

---
+++ src/components/AdviceGenerator/AdviceGenerator.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./AdviceGenerator.css";
+
+const AdviceGenerator = () => {
+    return (
+        <div className="generator-ctnr">
+            <p className="advice-id">advice #117</p>
+            <p className="advice-quote">
+                “It is easy to sit up and take notice, what's difficult is
+                getting up and taking action.”
+            </p>
+
+            <div className="svg" />
+        </div>
+    );
+};
+
+export default AdviceGenerator;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AdviceGenerator from "../../components/AdviceGenerator/AdviceGenerator";
+
+import "./Home.css";
+
+const Home = () => {
+    return (
+        <main className="global-ctnr">
+            <AdviceGenerator />
+        </main>
+    );
+};
+
+export default Home;
